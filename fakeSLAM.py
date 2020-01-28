8d23b2ae121796a8c13bad03bca35f371aa1cd1c
Cosette Craig <cosettecraig@Cosettes-MacBook-Pro.local>
2020-01-27 20:49:50 -0500

    Working pointcloud creation

--- fakeSLAM.py
+++ fakeSLAM.py
@@ -19,109 +19,87 @@ def noise(matrix):
     return matrix
 
 #makes a truth
-def truth(z_range, y_range): #some sort of blocky terrain
-    z = np.linspace(0,z_range,z_range+1)
-    y = np.random.randint(y_range, size = z.shape[0])
-    z1 = np.repeat(z,2)[1:]
+def truth(x_range, y_range): #some sort of blocky terrain
+    x = np.linspace(0,x_range,x_range+1)
+    y = np.random.randint(y_range, size = np.size(x))
+    x1 = np.repeat(x,2)[1:]
     y1 = np.repeat(y,2)[:-1]
-    
-    plt.plot(z1,y1)
-    
-    return np.concatenate(([z1],[y1]), axis = 0)
-
-def plot(z_max, y_max, init_angle, init_x, init_height):
-	truth(z_max, y_max)
-	plt.plot(init_x, init_height, 'r+')
-
-	FOV = 57
-	angle_min = init_angle-(FOV/2)
-	angle_max = init_angle+(FOV/2)
-
-	z_range = np.linspace(0,z_max,z_max*10+1)
-	y_range = np.linspace(0,y_max,y_max*10+1)
-	slope_min = m.tan(m.radians(angle_min))
-	slope_max = m.tan(m.radians(angle_max))
-
-	min_line = slope_min*z_range+init_height
-	pos_min = min_line[min_line>=0]
-	z_min = z_range[0:pos_min.size]
-	plt.plot(z_min, pos_min)
-
-	max_line = slope_max*z_range+init_height
-	pos_max = max_line[max_line>=0]
-	z_max = z_range[0:pos_max.size]
-	plt.plot(z_max, pos_max)
-
-	plt.plot(z_range, max_line)
-	plt.show()
-plot(20,10,init_angle, init_x, init_height)
+    i = 0
+    for i in range(i, np.size(x1), 2):
+    	x1[i] += 0.5
+    	i += 2
+    plt.plot(x1,y1)
+    return np.concatenate(([x1],[y1]), axis = 0)
 
 def get_intersect(a1, a2, b1, b2):
-    """ 
-    Returns the point of intersection of the lines passing through a2,a1 and b2,b1.
-    a1: [x, y] a point on the first line
-    a2: [x, y] another point on the first line
-    b1: [x, y] a point on the second line
-    b2: [x, y] another point on the second line
-    """
     s = np.vstack([a1,a2,b1,b2])        # s for stacked
     h = np.hstack((s, np.ones((4, 1)))) # h for homogeneous
-    l1 = np.cross(h[0], h[1])	       # get first line
+    l1 = np.cross(h[0], h[1])           # get first line
     l2 = np.cross(h[2], h[3])           # get second line
     x, y, z = np.cross(l1, l2)          # point of intersection
     if z == 0:                          # lines are parallel
-        pass
+        return (float('inf'), float('inf'))
+    return np.array([x/z, y/z])
 
-    x_range = np.arange(a1[0],a2[0],0.1)
-    y_range = np.arange(a1[1], a2[1],0.1)
-    if x_range.size == 0:
-    	if np.around([y/z]) in y_range:
-    		return np.array([x/z, y/z])
-    if y_range.size == 0:
-    	if np.around([x/z]) in x_range:
-    		return np.array([x/z, y/z])
-    elif np.around([x/z]) in x_range and np.around([y/z]) in y_range:
-    	return np.array([x/z, y/z])
-    else:
-    	pass
+# #makes a pointcloud of the truth terrain
+def pointcloud_generator(init_angle, init_height, init_x, truth):
+    pointcloud_x = np.array([])
+    pointcloud_y = np.array([])
+    FOV = 57
+    angle_min = init_angle-(FOV/2)
+    angle_max = init_angle+(FOV/2)
+    origin = [init_x, init_height]
+    
+    i = angle_min
+    j = i-angle_min
 
-def test_intersect():
-	np.testing.assert_equal(get_intersect([0,0],[1,1],[0,1],[1,0]), [0.5, 0.5])
-	assert get_intersect([0,0],[1,1],[2,2],[3,4]) == None
-	#vertical line intersect
-	np.testing.assert_equal(get_intersect([0,1],[4,1],[0,0],[5,5]), [1., 1.])
+    for i in range(angle_min, angle_max):
+        same_j = False
+        slope = m.tan(m.radians(i))
+        ray_point = [init_x+40, ((init_x+40)*slope)+18]
+        plt.plot([0,ray_point[0]],[18,ray_point[1]])
+        while same_j == False and j <= np.size(truth, 1)-1:
+            j_start = [truth[0, j], truth[1, j]]
+            j_end = [truth[0, j+1], truth[1, j+1]]
+            intersect = get_intersect(j_start, j_end, origin, ray_point)
+            if intersect[0]>j_end[0] or intersect[1]>j_end[1]:
+                j += 1
+            else:
+                same_j = True
+                pointcloud_x = np.append(pointcloud_x, intersect[0])
+                pointcloud_y = np.append(pointcloud_y, intersect[1])
 
-	print("Passed!")
-test_intersect()
+    return np.array([pointcloud_x, pointcloud_y])
 
-#makes a pointcloud of the truth terrain
-# truth = truth()
-# def pointcloud(init_angle, init_height, init_x, truth):
-#     pointcloud = np.array([])
-#     FOV = 57
-#     angle_min = init_angle-(FOV/2)
-#     angle_max = init_angle+(FOV/2)
-#     origin = [init_x, init_height]
-#     for angle in np.arange(angle_min, angle_max):
-#         slope = m.tan(m.radians(angle))
-#         ray_point = [init_x+1, (init_x+1)*slope]
-#         # find the closest set of points to ray, a1, a2
-#         intersect = get_intersect(a1,a2,origin,ray_point)
-#         if intersect is not None:
-#         	pointcloud = np.append(pointcloud,, axis=0)
+def plot(x_max, y_max, init_angle, init_x, init_height, truth):
+    pointcloud = pointcloud_generator(init_angle, init_height, init_x, truth)
+    plt.plot(init_x, init_height, 'r+')
+    plt.plot(pointcloud[0], pointcloud[1], 'ro')
 
+    FOV = 57
+    angle_min = init_angle-(FOV/2)
+    angle_max = init_angle+(FOV/2)
 
-#         # i = 0
-#         # for i in range(np.size(truth,1)):
-#         #     coordinate = truth[:,i]
-#         #     z = round(coordinate[0],1)
-#         #     y = round(coordinate[1],1)
-#         #     if (pointIsOnLine(slope,0,z,y)):
-#         #         pointcloud = np.append(pointcloud,[z, y], axis=0)
-#         #         i+=1
-#         #     else:
-#         #         i+=1
-#     return pointcloud
+    x_range = np.linspace(0,x_max,x_max*10+1)
+    y_range = np.linspace(0,y_max,y_max*10+1)
+    slope_min = m.tan(m.radians(angle_min))
+    slope_max = m.tan(m.radians(angle_max))
+
+    min_line = slope_min*x_range+init_height
+    pos_min = min_line[min_line>=0]
+    x_min = x_range[0:pos_min.size]
+    plt.plot(x_min, pos_min)
+
+    max_line = slope_max*x_range+init_height
+    pos_max = max_line[max_line>=0]
+    x_fov_max = x_range[0:pos_max.size]
+    plt.plot(x_fov_max, pos_max)
+    plt.xlim(-1, x_max)
+    plt.ylim(-1, init_height+1)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.show()
+
+plot(50,4,init_angle, init_x, init_height, truth(50,4))
 
 def initialize_camera():
     # start the frames pipe
